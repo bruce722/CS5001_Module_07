9a8f121a52a0524e5bc266acb6b078eab75553c9
bruce722 <huangchuanzhao0722@gmail.com>
2022-10-30 00:53:14 -0700

    choose.py | first submission

---
+++ choose.py
@@ -0,0 +1,11 @@
+def choose(n,k):
+    if k==0:
+        return 1
+    elif n<k:
+        return 0
+    else:
+        return choose(n-1, k-1) + choose(n-1, k)
+
+
+if __name__ == '__main__':
+    print(choose(6, 2))
