819f107bb6c0153421f543709a75d0bf4cd6d00b
bruce722 <huangchuanzhao0722@gmail.com>
2022-10-30 01:03:45 -0700

    find_max.py | fix negative max numbers

--- find_max.py
+++ find_max.py
@@ -1,13 +1,16 @@
+import sys
+
+
 def find_max(num_list):
     return find_max_helper(num_list, 0)
 
 
 def find_max_helper(num_list, index):
     if index >= len(num_list):
-        return 0
+        return -sys.maxsize - 1
     return max(find_max_helper(num_list, index + 1), num_list[index])
 
 
 if __name__ == '__main__':
-    num_list = [3, 5, 7, 6, 2, 1, 4]
+    num_list = [-3, -5, -7, -6, -2, -1, -4]
     print(find_max(num_list))
