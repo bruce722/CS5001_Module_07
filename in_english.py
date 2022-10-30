db55c9974981809372166364663230e6bc1b9411
bruce722 <huangchuanzhao0722@gmail.com>
2022-10-30 00:50:43 -0700

    in_english.py | first submission

---
+++ in_english.py
@@ -0,0 +1,11 @@
+num_str_arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+def in_english(num):
+    num = int(num)
+    if num < 10:
+        return num_str_arr[num]
+    return in_english(num / 10) + " " + num_str_arr[num % 10]
+
+
+if __name__ == '__main__':
+    print(in_english(153))
